3f46b5173631d7a48402bbd651fcc4086278d5b6
yaacov <yaacovayache@gmail.com>
2019-12-02 17:38:46 +0200

    new api key

--- src/const/weather-api.ts
+++ src/const/weather-api.ts
@@ -1,8 +1,7 @@
 export const WeatherApi = {
   urlLocation: 'http://dataservice.accuweather.com/locations/v1/',
   urlForecast: 'http://dataservice.accuweather.com/forecasts/v1/',
-  // key: 'AU71KPKOD19cWO5yvMyDHPWXvVSeGAkH',
-  key: 'U9q954aivddGbMLQPUKiBGWD02cug9a0',
+  key: 'AU71KPKOD19cWO5yvMyDHPWXvVSeGAkH',
   paramKeys: {
     cities : 'cities',
     autocomplete : 'autocomplete',
